104cff5b90e71b93e81401c691d4b6d293cc1877
artur <artur.brogowicz@gmail.com>
2024-12-15 18:18:42 +0100

    Inicjalizacja

---
+++ src/main/java/com/badabdd/beautysal00n/BeautySal00nApplication.java
@@ -0,0 +1,13 @@
+package com.badabdd.beautysal00n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySal00nApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySal00nApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/badabdd/beautysal00n/BeautySal00nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badabdd.beautysal00n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySal00nApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
