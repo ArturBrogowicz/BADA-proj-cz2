dc411b9291d96d74333f2fb5c3b4fc4401d51f32
artur <artur.brogowicz@gmail.com>
2025-01-06 21:54:33 +0100

    przykład frontendu w ajax

--- src/main/java/com/badabdd/beautysal00n/controllers/PracownicyManagementController.java
+++ src/main/java/com/badabdd/beautysal00n/controllers/PracownicyManagementController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @RequestMapping("admin/pracownicy/")
 public class PracownicyManagementController {
