7ba31a0c81cf6f0f84a2636c90adeadab86892eb
pdlc-t <ptdobrosielski@gmail.com>
2025-01-15 20:06:50 +0100

    Naprawa CORS chyba

--- src/main/java/com/badabdd/beautysal00n/config/SecurityConfig.java
+++ src/main/java/com/badabdd/beautysal00n/config/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,6 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Wyłączenie ochrony CORS
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
@@ -39,5 +45,26 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // Zezwolenie na dowolne źródło
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        // Zezwolenie na dowolne nagłówki
+        configuration.setAllowedHeaders(List.of("*"));
+
+        // Zezwolenie na dowolne metody HTTP
+        configuration.setAllowedMethods(List.of("*"));
+
+        // Zezwolenie na przesyłanie poświadczeń
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
